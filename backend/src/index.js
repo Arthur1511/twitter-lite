9bf28b9f14b9320f56bc38a2abb11be901ea90c1
Arthur1511 <arthur.bernardo1511@gmail.com>
2020-01-25 11:17:35 -0300

    backend created

---
+++ backend/src/index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
+
+mongoose.connect(
+  "mongodb+srv://arthur:arthur15@devradar-nrfhl.gcp.mongodb.net/twitter-lite?retryWrites=true&w=majority",
+  { useNewUrlParser: true, useUnifiedTopology: true }
+);
+
+app.use((req, res, next) => {
+  req.io = io;
+  return next();
+});
+
+app.use(cors());
+app.use(express.json());
+app.use(require("./routes"));
+
+server.listen(3000, () => {
+  console.log("Server started");
+});
